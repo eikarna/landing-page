0e23110d499b87c3cfddb19943806299a63daea3
Eikarna <nixic0@proton.me>
2024-05-11 22:28:11 +0700

    fixx

--- next.config.js
+++ next.config.js
@@ -3,15 +3,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const baseUrl = '';
+
 module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['.'],
-  },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: baseUrl,
+  env: {
+    baseUrl,
+  },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
-  reactStrictMode: true
+  reactStrictMode: true,
 });
